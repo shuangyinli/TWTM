9350dccb00766caf39a66c10e95d90da84d1ecb0
Shuangyin Li <lshuangyin@gmail.com>
2016-01-18 22:14:57 +0800

    Update utils.h

--- src/utils.h
+++ src/utils.h
@@ -1,9 +1,3 @@
-/*============================================================================= 
-#  Author:          JiefeiLi
-#  Email:           lijiefei@mail2.sysu.edu.cn 
-#  School:          Sun Yat-sen University
-=============================================================================*/ 
-
 #ifndef UTILS_H
 #define UTILS_H
 
